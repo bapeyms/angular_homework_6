d9a0debd9517e9e821954e673e47338f6aed62c1
serdka99 <serdka99@29-8>
2026-05-11 16:22:04 +0300

    start

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,5 +1,6 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Form } from './form/form';
 
 @Component({
   selector: 'app-root',
---
+++ src/app/form/form.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-form',
+  imports: [],
+  templateUrl: './form.html',
+  styleUrl: './form.css',
+})
+export class Form {}
