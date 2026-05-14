d2d92c9da86d5249172c05aa5620bd582b616bc3
Katherine <katerynaunik@gmail.com>
2026-05-14 15:40:57 +0300

    task 2

--- src/app/form/form.ts
+++ src/app/form/form.ts
@@ -1,27 +1,49 @@
 import { Component } from '@angular/core';
-import { 
-  FormBuilder, 
-  FormGroup, 
-  FormsModule, 
-  ReactiveFormsModule,
-  Validators } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+import {
+  FormArray,
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators
+} from '@angular/forms';
 
 @Component({
   selector: 'app-form',
-  imports: [FormsModule, ReactiveFormsModule, CommonModule],
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './form.html',
   styleUrl: './form.css',
 })
 export class Form {
-
+  
   myForm: FormGroup;
 
   constructor(private fb: FormBuilder) {
+
     this.myForm = this.fb.group({
       email: ['', [Validators.required, Validators.email]],
-      password: ['', [Validators.required, Validators.minLength(6)]]
+
+      password: ['', [
+        Validators.required,
+        Validators.minLength(6)
+      ]],
+
+      agree: [false, Validators.requiredTrue],
+
+      hobbies: this.fb.array([])
     });
+  }
+
+  get hobbies(): FormArray {
+    return this.myForm.get('hobbies') as FormArray;
+  }
+
+  addHobby() {
+    this.hobbies.push(this.fb.control(''));
+  }
+
+  removeHobby(index: number) {
+    this.hobbies.removeAt(index);
   }
 
   onSubmit() {
