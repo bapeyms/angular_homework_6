b19a804b86b1055930dce12650a97043cb83cfc0
Katherine <katerynaunik@gmail.com>
2026-05-14 15:29:57 +0300

    task 2

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,15 +1,17 @@
-import { Component, signal } from '@angular/core';
+import { Component} from '@angular/core';
+
 import { RouterOutlet } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 import { ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+
 import { Form } from './form/form';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Form, FormsModule, ReactiveFormsModule],
+  imports: [RouterOutlet, Form, FormsModule, ReactiveFormsModule, CommonModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('homework');
 }
--- src/app/form/form.ts
+++ src/app/form/form.ts
@@ -1,19 +1,32 @@
 import { Component } from '@angular/core';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { 
+  FormBuilder, 
+  FormGroup, 
+  FormsModule, 
+  ReactiveFormsModule,
+  Validators } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-form',
-  imports: [FormsModule],
+  imports: [FormsModule, ReactiveFormsModule, CommonModule],
   templateUrl: './form.html',
   styleUrl: './form.css',
 })
 export class Form {
-  user = {
-    email: '',
-    password: ''
+
+  myForm: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.myForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]]
+    });
   }
 
   onSubmit() {
-    console.log(this.user)
+    if (this.myForm.valid) {
+      console.log(this.myForm.value);
+    }
   }
 }
